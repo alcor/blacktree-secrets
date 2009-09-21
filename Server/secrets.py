673a24e8104c122857a05a6b7ed0c6a34ddc78c8
jnj <jnj@1ffa7bd4-6a3d-0410-ab8a-d1375621608d>
2009-09-21 02:07:18 +0000

    Updated appengine

--- Server/secrets.py
+++ Server/secrets.py
@@ -120,8 +120,13 @@ class Secret(search.SearchableModel):
     
   def display_icon(self):
       if self.display_bundle:
-        return self.display_bundle
-      return self.bundle
+        bundle = self.display_bundle
+      else:
+        bundle = self.bundle
+      
+      if bundle == ".GlobalPreferences":
+        bundle = "GlobalPreferences"
+      return bundle
       
   def display_app(self):
     bundle = self.bundle
@@ -133,21 +138,21 @@ class Secret(search.SearchableModel):
     #   if (len(display_bundle) > 0):
     #     icon = self.display_bundle
     #     icon = icon.split('/').last
-    
- 
     # if (display_bundle == "prefPane" | display_bundle == "editor"  | display_bundle == "bundle" | display_bundle == "launcher"):
     #    # display_bundle = self.display_bundle.split('.')[-2]
     #    display_bundle = self.display_bundle.split('/')[-1]
     #   
+    
     if bundle == "GlobalPreferences":
       bundle = "Every App"
   
-    if bundle == "kCFPreferencesAnyApplication":
+    if bundle == "kCFPreferencesCurrentApplication":
       bundle = "Any App"
       
     if self.display_bundle:
       bundle = display_bundle.split('.')[-1]
     return bundle
+    
     #  def put_value (key, type, xml)
     # 
     #     value = eval "self." + key
@@ -164,14 +169,15 @@ class SecretForm(djangoforms.ModelForm):
     
 class PlistSecret(webapp.RequestHandler):
   def get(self):    
-    self.response.headers['Secrets-Version'] = "1.0.5"
+    self.response.headers['Secrets-Version'] = "1.0.6"
     self.response.headers['Content-Type'] = 'text/xml; charset=utf-8'
     output = memcache.get("plist")
     if output is None:
       plist_content = ''
-      secrets = Secret.all().order('-created_at')
+      secrets = Secret.all()
+      secrets.filter('deleted ==', False)
       count = 0;
-      for i in range (0, 3):
+      for i in range (0, 4):
         some_secrets = secrets.fetch(200, 200*i)
         this_count = len(some_secrets)
         count += this_count
@@ -182,6 +188,23 @@ class PlistSecret(webapp.RequestHandler):
       memcache.add("plist", output)
       self.response.headers['Cached'] = 'yes'
     self.response.out.write(output)
+class TextSecret(webapp.RequestHandler):
+  def get(self):    
+    output = memcache.get("txt")    
+    self.response.headers['Content-Type'] = 'text; charset=utf-8'
+    if output is None:
+      plist_content = ''
+      secrets = Secret.all()
+      secrets.filter('deleted ==', False)
+      count = 0;
+      for i in range (0, 4):
+        some_secrets = secrets.fetch(200, 200*i)
+        this_count = len(some_secrets)
+        count += this_count
+        for secret in some_secrets:
+          self.response.out.write('%s\t%s\r' % (secret.bundle, secret.keypath))
+        if this_count == 0:
+          break
 
 class MainPage(webapp.RequestHandler):
   def get(self):
@@ -189,6 +212,7 @@ class MainPage(webapp.RequestHandler):
     
     showall = self.request.get('show') == 'all'
     showrecent = self.request.get('show') == 'recent'
+    showdeleted = self.request.get('show') == 'deleted'
     search_string = self.request.get('search')
 
     warning = None
@@ -231,6 +255,11 @@ class MainPage(webapp.RequestHandler):
       elif showall == True:
         title = "All Secrets: page %d" % (page + 1)
         query = db.GqlQuery("SELECT * FROM Secret WHERE deleted = False "
+                              "ORDER BY created_at DESC")
+        secrets = query.fetch(100, 100 * page)
+      elif showdeleted == True:
+        title = "All Secrets: page %d" % (page + 1)
+        query = db.GqlQuery("SELECT * FROM Secret WHERE deleted = True "
                               "ORDER BY created_at DESC")
         secrets = query.fetch(100, 100 * page)
       elif search_string is not None and len(search_string) > 3:
@@ -312,14 +341,19 @@ class EditSecret(webapp.RequestHandler):
   
   def post(self):
     id = self.request.get('_id') 
-    
+    dups = None
     if id:
       item = Secret.get(db.Key.from_path('Secret', int(id)))
       data = SecretForm(data=self.request.POST, instance=item)
     else:
+      dups = db.GqlQuery("SELECT * FROM Secret WHERE bundle = :1 "
+                          "AND keypath = :2 ",
+                          self.request.get('bundle'), self.request.get('keypath'))
       data = SecretForm(data = self.request.POST)
-    
-    if data.is_valid():
+      self.response.out.write(dups.count())
+
+      
+    if data.is_valid() and (dups == None or dups.count() == 0):
       # Save the data, and redirect to the view page
       entity = data.save(commit=False)
       if not id:
@@ -333,7 +367,7 @@ class EditSecret(webapp.RequestHandler):
       if id:
         self.response.out.write(template.render('form.html', {'form':data, 'id':id}))
       else:
-        self.response.out.write(template.render('form.html', {'form':data}))
+        self.response.out.write(template.render('form.html', {'form':data, 'dups':dups}))
       
 class FlushSecrets(webapp.RequestHandler):
   def get(self):
@@ -359,7 +393,7 @@ class RSSNewSecret(webapp.RequestHandler):
       output = template.render("rss.xml", {'secrets':secrets})
       memcache.add("rss-new", output)
 
-    self.response.headers['Content-Type'] = 'text/rss+xml; charset=utf-8'      
+    # self.response.headers['Content-Type'] = 'text/rss+xml; charset=utf-8'      
     self.response.out.write(output)
     
 class RSSUpdatedSecret(webapp.RequestHandler):
@@ -417,6 +451,7 @@ def main():
                                         ('/rss/new', RSSNewSecret),
                                         ('/delete',DeleteSecret),
                                         ('/edit', EditSecret),
+                                        ('/txt', TextSecret),
                                         ('/plist', PlistSecret),
                                         # ('/backup', Backup),
                                         ('/flush', FlushSecrets),
